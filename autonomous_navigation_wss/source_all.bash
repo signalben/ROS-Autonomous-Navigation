71e9c4c32099f2bb8e098b16aa4effcdb86cf217
signalben <bevans442@live.com>
2021-05-17 01:49:45 +0100

    First commit

---
+++ autonomous_navigation_wss/source_all.bash
@@ -0,0 +1,12 @@
+#! /bin/bash 
+
+source turtlebot/devel/setup.bash --extend
+source turtlebot_simulation/devel/setup.bash --extend
+source turtlebot_interaction/devel/setup.bash --extend
+source mapping/devel/setup.bash --extend
+source navigation/devel/setup.bash --extend
+source exploration/devel/setup.bash --extend
+
+
+
+
